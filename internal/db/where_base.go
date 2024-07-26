5997d914f97db33cafd3fd4fdfed134d6aaa9e59
Masami Murai <masami_murai@faciam.net>
2024-07-27 03:04:52 +0900

    [modify] feat: Implement support for WHERE BETWEEN condition in query builder

--- internal/db/where_base.go
+++ internal/db/where_base.go
@@ -23,205 +23,166 @@ func (wb *WhereBaseBuilder) Where(sb *strings.Builder, wg *[]structs.WhereGroup)
 	}
 
 	// WHERE
-	hasCondition := false
-	for _, cg := range *wg {
-		if len(cg.Conditions) > 0 {
-			hasCondition = true
-			break
-		}
-	}
-
-	if hasCondition {
+	if hasCondition(*wg) {
 		sb.WriteString(" WHERE ")
 	}
 
 	values := make([]interface{}, 0)
 
-	sep := ""
-	notSep := ""
 	for i, cg := range *wg {
 		if len(cg.Conditions) == 0 {
 			continue
 		}
 
-		// AND, OR by ConditionGroup
-		if cg.IsDummyGroup {
-			sep = ""
-		} else if cg.Operator == consts.LogicalOperator_AND {
-			sep = " AND "
-		} else if cg.Operator == consts.LogicalOperator_OR {
-			sep = " OR "
-		}
-
 		if i > 0 {
-			sb.WriteString(sep)
+			sb.WriteString(getConditionGroupSeparator(cg, i))
 		}
 
-		parenthesesOpen := ""
-		parenthesesClose := ""
-		op := ""
-
-		if !cg.IsDummyGroup {
-			if cg.IsNot {
-				notSep = "NOT "
-			} else {
-				notSep = ""
-			}
-			parenthesesOpen = "("
-			parenthesesClose = ")"
-		}
-
-		sb.WriteString(notSep)
-		sb.WriteString(parenthesesOpen)
+		sb.WriteString(getNotSeparator(cg))
+		sb.WriteString(getParenthesesOpen(cg))
 
 		for j, c := range cg.Conditions {
-			convertedColumn := c.Column
-			if i > 0 && j == 0 && cg.IsDummyGroup {
-				if c.Operator == consts.LogicalOperator_AND {
-					op = " AND "
-				} else if c.Operator == consts.LogicalOperator_OR {
-					op = " OR "
-				}
+			if j > 0 || (i > 0 && j == 0 && cg.IsDummyGroup) {
+				sb.WriteString(getConditionOperator(c))
 			}
 
-			if c.Query != nil {
-				condQuery := convertedColumn + " " + c.Condition
-
-				// create subquery
-				b := &BaseQueryBuilder{}
-				sqQuery, sqValues := b.Build("", c.Query)
-
-				if c.Operator == consts.LogicalOperator_AND {
-					if op != "" {
-						op = " AND "
-					}
-					sb.WriteString(op + condQuery + " (" + sqQuery + ")")
-					if op == "" {
-						op = " AND "
-					}
-				} else if c.Operator == consts.LogicalOperator_OR {
-					if op != "" {
-						op = " OR "
-					}
-					sb.WriteString(op + condQuery + " (" + sqQuery + ")")
-					if op == "" {
-						op = " OR "
-					}
-				}
-
-				values = append(values, sqValues...)
-			} else if c.Exists != nil {
-				condQuery := c.Condition
-
-				// create subquery
-				b := &BaseQueryBuilder{}
-				sqQuery, sqValues := b.Build("", c.Exists.Query)
-
-				if c.Operator == consts.LogicalOperator_AND {
-					if op != "" {
-						op = " AND "
-					}
-					sb.WriteString(op + condQuery + " (" + sqQuery + ")")
-					if op == "" {
-						op = " AND "
-					}
-				} else if c.Operator == consts.LogicalOperator_OR {
-					if op != "" {
-						op = " OR "
-					}
-					sb.WriteString(op + condQuery + " (" + sqQuery + ")")
-					if op == "" {
-						op = " OR "
-					}
-				}
-
-				values = append(values, sqValues...)
-
-			} else if c.Between != nil {
-				wsb := strings.Builder{}
-				wsb.Grow(consts.StringBuffer_Where_Grow)
-				if c.Between.IsColumn {
-					wsb.WriteString(convertedColumn + " " + c.Condition + " " + c.Between.From.(string) + " AND " + c.Between.To.(string))
-				} else {
-					wsb.WriteString(convertedColumn + " " + c.Condition + " ? AND ?")
-					values = append(values, c.Between.From, c.Between.To)
-				}
-				condQuery := wsb.String()
-				wsb.Reset()
-
-				if c.Operator == consts.LogicalOperator_AND {
-					if op != "" {
-						op = " AND "
-					}
-					sb.WriteString(op + condQuery)
-					if op == "" {
-						op = " AND "
-					}
-				} else if c.Operator == consts.LogicalOperator_OR {
-					if op != "" {
-						op = " OR "
-					}
-					sb.WriteString(op + condQuery)
-					if op == "" {
-						op = " OR "
-					}
-				}
-			} else {
-				raw := c.Raw
-				wsb := strings.Builder{}
-				wsb.Grow(consts.StringBuffer_Where_Grow)
-				if raw != "" {
-					wsb.WriteString(raw)
-				} else {
-					wsb.WriteString(convertedColumn + " " + c.Condition)
-					if c.ValueColumn != "" {
-						wsb.WriteString(" " + c.ValueColumn)
-					} else if c.Value != nil {
-						if len(c.Value) > 1 {
-							wsb.WriteString(" (")
-							for k := 0; k < len(c.Value); k++ {
-								if k > 0 {
-									wsb.WriteString(", ")
-								}
-								wsb.WriteString("?")
-							}
-							wsb.WriteString(")")
-						} else {
-							wsb.WriteString(" ?")
-						}
-					}
-				}
-				condQuery := wsb.String()
-				wsb.Reset()
-
-				if c.Operator == consts.LogicalOperator_AND {
-					if op != "" {
-						op = " AND "
-					}
-					sb.WriteString(op + condQuery)
-					if len(c.Value) > 0 {
-						values = append(values, c.Value...)
-					}
-					if op == "" {
-						op = " AND "
-					}
-				} else if c.Operator == consts.LogicalOperator_OR {
-					if op != "" {
-						op = " OR "
-					}
-					sb.WriteString(op)
-					sb.WriteString(condQuery)
-					if len(c.Value) > 0 {
-						values = append(values, c.Value...)
-					}
-					if op == "" {
-						op = " OR "
-					}
-				}
+			switch {
+			case c.Query != nil:
+				values = append(values, processSubQuery(sb, c)...)
+			case c.Exists != nil:
+				values = append(values, processExistsQuery(sb, c)...)
+			case c.Between != nil:
+				values = append(values, processBetweenCondition(sb, c)...)
+			default:
+				values = append(values, processRawCondition(sb, c)...)
 			}
 		}
-		sb.WriteString(parenthesesClose)
+		sb.WriteString(getParenthesesClose(cg))
 	}
 
+	return values
+}
+
+func hasCondition(wg []structs.WhereGroup) bool {
+	for _, cg := range wg {
+		if len(cg.Conditions) > 0 {
+			return true
+		}
+	}
+	return false
+}
+
+func getConditionGroupSeparator(cg structs.WhereGroup, index int) string {
+	if cg.IsDummyGroup {
+		return ""
+	}
+	if index == 0 {
+		return ""
+	}
+	switch cg.Operator {
+	case consts.LogicalOperator_AND:
+		return " AND "
+	case consts.LogicalOperator_OR:
+		return " OR "
+	}
+	return ""
+}
+
+func getNotSeparator(cg structs.WhereGroup) string {
+	if cg.IsNot {
+		return "NOT "
+	}
+	return ""
+}
+
+func getParenthesesOpen(cg structs.WhereGroup) string {
+	if cg.IsDummyGroup {
+		return ""
+	}
+	return "("
+}
+
+func getParenthesesClose(cg structs.WhereGroup) string {
+	if cg.IsDummyGroup {
+		return ""
+	}
+	return ")"
+}
+
+func getConditionOperator(c structs.Where) string {
+	switch c.Operator {
+	case consts.LogicalOperator_AND:
+		return " AND "
+	case consts.LogicalOperator_OR:
+		return " OR "
+	}
+	return ""
+}
+
+func processSubQuery(sb *strings.Builder, c structs.Where) []interface{} {
+	condQuery := c.Column + " " + c.Condition
+	b := &BaseQueryBuilder{}
+	sqQuery, sqValues := b.Build("", c.Query)
+
+	sb.WriteString(condQuery + " (" + sqQuery + ")")
+	return sqValues
+}
+
+func processExistsQuery(sb *strings.Builder, c structs.Where) []interface{} {
+	condQuery := c.Condition
+	b := &BaseQueryBuilder{}
+	sqQuery, sqValues := b.Build("", c.Exists.Query)
+
+	sb.WriteString(condQuery + " (" + sqQuery + ")")
+	return sqValues
+}
+
+func processBetweenCondition(sb *strings.Builder, c structs.Where) []interface{} {
+	wsb := strings.Builder{}
+	wsb.Grow(consts.StringBuffer_Where_Grow)
+	values := make([]interface{}, 0, 2)
+	if c.Between.IsColumn {
+		wsb.WriteString(c.Column + " " + c.Condition + " " + c.Between.From.(string) + " AND " + c.Between.To.(string))
+	} else {
+		wsb.WriteString(c.Column + " " + c.Condition + " ? AND ?")
+		values = []interface{}{c.Between.From, c.Between.To}
+	}
+
+	condQuery := wsb.String()
+
+	sb.WriteString(condQuery)
+	return values
+}
+
+func processRawCondition(sb *strings.Builder, c structs.Where) []interface{} {
+	wsb := strings.Builder{}
+	wsb.Grow(consts.StringBuffer_Where_Grow)
+
+	if c.Raw != "" {
+		wsb.WriteString(c.Raw)
+	} else {
+		wsb.WriteString(c.Column + " " + c.Condition)
+		if c.ValueColumn != "" {
+			wsb.WriteString(" " + c.ValueColumn)
+		} else if c.Value != nil {
+			if len(c.Value) > 1 {
+				wsb.WriteString(" (")
+				for k := 0; k < len(c.Value); k++ {
+					if k > 0 {
+						wsb.WriteString(", ")
+					}
+					wsb.WriteString("?")
+				}
+				wsb.WriteString(")")
+			} else {
+				wsb.WriteString(" ?")
+			}
+		}
+	}
+
+	condQuery := wsb.String()
+	values := c.Value
+
+	sb.WriteString(condQuery)
 	return values
 }
