7893e93be6a102f9de2c688b6f882cd60156552a
Masami Murai <masami_murai@faciam.net>
2024-08-02 17:17:31 +0900

    [add update sample 2 and 3

--- example/1_select/go.mod
+++ example/1_select/go.mod
@@ -2,4 +2,4 @@ module github.com/faciam-dev/goquent-query-builder/example/0_basic
 
 go 1.22.5
 
-require github.com/faciam-dev/goquent-query-builder v0.0.0-20240731144822-b85b3e8fe3f4
+require github.com/faciam-dev/goquent-query-builder v0.0.0-20240802075440-b3e3ba5ce24a
---
+++ example/3_select_join/main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/faciam-dev/goquent-query-builder/internal/cache"
+	"github.com/faciam-dev/goquent-query-builder/internal/db"
+	"github.com/faciam-dev/goquent-query-builder/pkg/api"
+)
+
+func main() {
+	// Initialize database strategy
+	dbStrategy := db.NewMySQLQueryBuilder()
+
+	asyncCache := cache.NewAsyncQueryCache(100)
+
+	// Simple query with JOIN
+	//
+	// SELECT users.id, users.name AS name
+	// FROM users
+	// JOIN profiles ON users.id = profiles.user_id
+	// ORDER BY users.name ASC
+	//
+
+	// Executing query: SELECT users.id, users.name AS name FROM users JOIN profiles ON users.id = profiles.user_id ORDER BY users.name ASC with values: []
+
+	qb := api.NewSelectQueryBuilder(dbStrategy, asyncCache).
+		Table("users").
+		Select("id", "users.name AS name").
+		Join("profiles", "users.id", "=", "profiles.user_id").
+		OrderBy("users.name", "ASC")
+
+	query, values, err := qb.Build()
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Print the query and values
+	fmt.Println("Executing query:", query, "with values:", values)
+
+	// Complex query with JoinQuery
+
+	// SELECT users.id, users.name AS name
+	// FROM users
+	// JOIN profiles ON users.id = profiles.user_id
+	// AND profiles.age > 18
+	// ORDER BY users.name ASC
+
+	// Executing query: SELECT users.id, users.name AS name FROM users JOIN profiles ON users.id = profiles.user_id AND profiles.age > ? ORDER BY users.name ASC with values: [18]
+
+	qb = api.NewSelectQueryBuilder(dbStrategy, asyncCache).
+		Table("users").
+		Select("id", "users.name AS name").
+		JoinQuery("profiles", func(b *api.JoinClauseQueryBuilder) {
+			b.On("users.id", "=", "profiles.user_id").
+				Where("profiles.age", ">", 18)
+		}).
+		OrderBy("users.name", "ASC")
+
+	query, values, err = qb.Build()
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Print the query and values
+
+	fmt.Println("Executing query:", query, "with values:", values)
+
+	// Query with multiple JOINs
+
+	// SELECT users.id, users.name AS name
+	// FROM users
+	// JOIN profiles ON users.id = profiles.user_id
+	// JOIN addresses ON users.id = addresses.user_id
+	// AND profiles.age > 18
+	// ORDER BY users.name ASC
+
+	// Executing query: SELECT users.id, users.name AS name FROM users JOIN profiles ON users.id = profiles.user_id JOIN addresses ON users.id = addresses.user_id AND profiles.age > ? ORDER BY users.name ASC with values: [18]
+
+	qb = api.NewSelectQueryBuilder(dbStrategy, asyncCache).
+		Table("users").
+		Select("id", "users.name AS name").
+		JoinQuery("profiles", func(b *api.JoinClauseQueryBuilder) {
+			b.On("users.id", "=", "profiles.user_id").
+				Where("profiles.age", ">", 18)
+		}).
+		Join("addresses", "users.id", "=", "addresses.user_id").
+		OrderBy("users.name", "ASC")
+
+	query, values, err = qb.Build()
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Print the query and values
+
+	fmt.Println("Executing query:", query, "with values:", values)
+
+	// Query with multiple JOINs and multiple conditions
+
+	// SELECT users.id, users.name AS name
+	// FROM users
+	// JOIN profiles ON users.id = profiles.user_id
+	// JOIN addresses ON users.id = addresses.user_id
+	// AND profiles.age > 18
+	// AND addresses.city = 'New York'
+	// ORDER BY users.name ASC
+
+	// Executing query: SELECT users.id, users.name AS name FROM users JOIN profiles ON users.id = profiles.user_id JOIN addresses ON users.id = addresses.user_id AND profiles.age > ? AND addresses.city = ? ORDER BY users.name ASC with values: [18 New York]
+
+	qb = api.NewSelectQueryBuilder(dbStrategy, asyncCache).
+		Table("users").
+		Select("id", "users.name AS name").
+		JoinQuery("profiles", func(b *api.JoinClauseQueryBuilder) {
+			b.On("users.id", "=", "profiles.user_id").
+				Where("profiles.age", ">", 18)
+		}).
+		JoinQuery("addresses", func(b *api.JoinClauseQueryBuilder) {
+			b.On("users.id", "=", "addresses.user_id").
+				Where("addresses.city", "=", "New York")
+		}).
+		OrderBy("users.name", "ASC")
+
+	query, values, err = qb.Build()
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Print the query and values
+
+	fmt.Println("Executing query:", query, "with values:", values)
+
+}
